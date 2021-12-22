71245f3ab78fc00da525d44a3937c2905a17a694
Micah Weston <micah.weston@usio.com>
2021-12-22 15:26:59 -0600

    working on a project

---
+++ BulkyBookWeb/Data/ApplicationDbContext.cs
@@ -0,0 +1,14 @@
+﻿using BulkyBookWeb.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace BulkyBookWeb.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+        }
+        // Creates our database table using our Category model
+        public DbSet<Category> Categories { get; set; } 
+    }
+}
